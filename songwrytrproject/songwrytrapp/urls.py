c94f9d4dcfbe6a0c6fcbf9d3d677ef3656f106f4
Landon Morgan <morganlandon@gmail.com>
2020-06-15 13:18:15 -0500

    writers CRUD functional and only displays user-specific data

--- songwrytrproject/songwrytrapp/urls.py
+++ songwrytrproject/songwrytrapp/urls.py
@@ -6,7 +6,7 @@ app_name = "songwrytrapp"
 urlpatterns = [
     path('', home, name='home'),
     path('writers/', writer_list, name='writers'),
-    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('django.contrib.auth.urls'), name='login'),
     path('logout/', logout_user, name='logout'),
     path('writer/form', writer_form, name='writer_form'),
     path('writers/<int:writer_id>/form/', writer_edit_form, name='writer_edit_form'),
